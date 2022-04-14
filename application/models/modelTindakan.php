80c98ae6a9662140556ad2d2bf71be87941c9781
yakalitam <robbypratama06@gmail.com>
2022-04-14 11:26:18 +0700

    update

---
+++ application/models/modelTindakan.php
@@ -0,0 +1,18 @@
+<?php
+class ModelTindakan extends CI_Model
+{
+    function getData()
+    {
+        return $this->db->get('tindakan')->result();
+    }
+
+    function search($keyword = null)
+    {
+        $this->db->select('*');
+        $this->db->from('tindakan');
+        if (!empty($keyword)) {
+            $this->db->like('namatindakan', $keyword);
+        }
+        return $this->db->get()->result_array();
+    }
+}
